71fefcc02c05637551ee39a8284cfea299bfb172
svassili <svassiliev1@brocku.ca>
2018-03-12 00:17:25 -0400

    Moving average

--- combine_bathymetry.py
+++ combine_bathymetry.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python	
-
-from mergepoints import merge_points
+from mergepoints import merge_points, sqdistance
 from stl import mesh
-import shapefile, fiona, csv, math, numpy, os
-
+import shapefile, fiona, csv, math, numpy, os, sys
 
 # This script reads depth points from csv and shape files, and 
 # combines them in a single shapefile. All files should be in
@@ -15,14 +13,24 @@ PerimeterFile = "Refined_Data/opinicon_perim_&_offset.dbf"
 SounderFile1 = "Initial_Data/opinicon_raw_gps_no_duplicates.shp"
 csvFile1 = "Initial_Data/20171021_location.csv"
 OutFile = "opinicon_combined_bathymetry."
-# Fill gaps between points separated by distance bigger than: 
-space = 5.0;
-# Multiply z b:
+# z scaling factor.
+# Output z will be multiplied by zmult.
 zmult = 40.0
-# Merge points separated by less than: 
+# Minimal allowed points separation.
+# Points separated by less than r will be merged. 
 r = 1.0
-# Maximum allowed depth spike 
-maxh = 2.0
+# Maximum iterrations
+ntries = 20
+# Moving average distance 
+n_av = 20.0
+# Maximum allowed depth peak height.
+# Positive and negative peaks with h < maxh and width = 1
+# will be replaced by average of z[i-1] and z[i+1]
+maxh = 1.0
+print "\nMinimal allowed points separation =", r
+print "Moving average distance =", n_av
+print "Maximum allowed peak height =",maxh
+print "Depth scaling factor =",zmult
 #------------------------------------------------------------
 SounderFile1 = WorkDir + SounderFile1
 csvFile1 = WorkDir + csvFile1
@@ -35,13 +43,11 @@ file=PerimeterFile
 sh = shapefile.Reader(file)
 sh_records = sh.shapeRecords()
 sh_shapes = sh.shapes()
-
 nShapes=range(len(sh_shapes))
 ii=0; j=0; ind=[]; ind.append(0)
 segments=[]
 holes=[]
 count=0
-
 for i in nShapes:
    n=len(sh_shapes[i].points)
    ii=ii+n; hx=0; hy=0
@@ -55,7 +61,6 @@ for i in nShapes:
    holes.append([hx,hy])
    z += sh_shapes[i].z
 print "\n<<< Reading perimeter >>>\n", os.path.basename(file)+",", len(x), "points\n"  
-
 #------------------------------------------------------------
 # <<<<<<<<<<<<<<<< Read raw sounder data: >>>>>>>>>>>>>>>>>> 
 #------------------------------------------------------------
@@ -74,20 +79,52 @@ for i in range(len(bt_shapes)):
 print "<<< Reading sounder data >>>\n", os.path.basename(file)+",", len(xt),"points\n" 
 # Find and delete outliers
 print "<<< Deleting spikes >>>"
-print '{0:6} {1:8} {2:6}'.format("     #","     ID","Height")
+print '{0:6} {1:8} {2:6}'.format("     #","     ID","Height+ " "Height-")
 c=0
 for i in range(1,len(xt)-1):
+   # spikes
     h = min(zt[i]-zt[i-1], zt[i]-zt[i+1])
-    if h > maxh:
+   # dips
+    g = min(-zt[i]+zt[i-1], -zt[i]+zt[i+1])   
+    if h > maxh or g > maxh:
         c+=1
-        print '{0:6} {1:8} {2:6}'.format(c,i,h)
+        print '{0:6} {1:8} {2:7} {3:7}'.format(c,i,h,g)
+        x2.append(xt[i]); y2.append(yt[i]);
+        z2.append((zt[i+1]+zt[i-1])*0.5)    
     else:
         x2.append(xt[i]); y2.append(yt[i]); z2.append(zt[i])
+
+# Moving average over a distance n_av
+print "\n<<< Computing moving average >>>" 
+n_av *= n_av
+for i in range(len(x2)):
+   j=1;k=0
+   tm=0.0
+   while i+j < len(x2): 
+      if sqdistance(x2,y2,i,i+j) < n_av:
+         tm += z2[i+j]
+         j += 1
+      else:
+         if k == i+j:
+           break     
+         z2[i] += tm
+         z2[i] /= j
+         k=i+j
+         break
+   if i + j == len(x):
+      break
+      
 # Merge closely spaced data points
 print "\n<<< Merging closely spaced data points >>>"
+count=0
 while True:
-  try:  
+  try:
+    if count > ntries:
+       print "Error: maximum iterrations reached"
+       print "Try to decrease minimal allowed points separation"
+       sys.exit()
     x2,y2,z2 = merge_points(x2,y2,z2,r)
+    count += 1
   except TypeError:
     break
 x += x2[:]; y += y2[:]; z += z2[:]
@@ -115,7 +152,8 @@ import triangle.plot
 print "<<< Constrained conforming Delaunay triangulation >>>"   
 print "... this may take a while ...\n"
 # Build array of segments
-for i in range(57):
+nPoly=len(nShapes)/2
+for i in range(nPoly):
    for j in range(ind[i],ind[i+1]-1):
       segments.append([j,j+1]) 
    segments.append([j+1,ind[i]])   
@@ -130,7 +168,7 @@ SEGM = numpy.ndarray(shape = (ns,2), dtype = int)
 for i in range(ns):
    SEGM[i,0] = segments[i][0]; SEGM[i,1] = segments[i][1]; 
 # number of holes (islands)
-ns=56
+ns=nPoly-1
 # perimeter comes first, so we skip it
 holes.pop(0)
 HOLES = numpy.ndarray(shape = (ns,2), dtype = float)
@@ -169,7 +207,6 @@ for i, f in enumerate(faces):
 # Write meshes to files
 bottom_msh.save('bottom_mesh.stl')
 top_msh.save('top_mesh.stl')
-
 #----------------------------------------------------------
 # <<<<<<<< Write output shapefile and projection >>>>>>>>>>
 #----------------------------------------------------------
@@ -178,21 +215,17 @@ ShapeType=shapefile.POINTZ
 w=shapefile.Writer(ShapeType)
 w.autobalance=1
 w.field("ID", "F",10,5)
-
 print "<<< Output >>>"
 print OutFile+"shp,", len(x), "points"
-
 # write as points
 pcount=0
 for i in range(len(x)):
   w.point(x[i],y[i],z[i]*zmult)
   w.record(z[i]*zmult)
   pcount += 1
-       
 for s in w.shapes():
   s.shapeType = ShapeType
 w.save(OutFile)
-
 # Write projection
 with fiona.open(PerimeterFile) as fp:
   prj=open(OutFile+"prj","w")
